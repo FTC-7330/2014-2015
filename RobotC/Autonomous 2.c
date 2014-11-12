725dcb4d7b7064935b580fa2138b3662ee6349cb
ngupta-iwasaki <ngupta-iwasaki@hotmail.com>
2014-11-11 23:08:48 -0800

    Added PID Control to Drive Motors
    Code Review: Rohan

--- RobotC/Autonomous 2.c
+++ RobotC/Autonomous 2.c
@@ -3,10 +3,10 @@
 #pragma config(Sensor, S2,     irSensor,       sensorHiTechnicIRSeeker1200)
 #pragma config(Sensor, S3,     sonar,          sensorSONAR)
 #pragma config(Sensor, S4,     gyro,           sensorI2CHiTechnicGyro)
-#pragma config(Motor,  mtr_S1_C1_1,     backRight,     tmotorTetrix, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     backLeft,      tmotorTetrix, openLoop, reversed, encoder)
-#pragma config(Motor,  mtr_S1_C2_1,     frontRight,    tmotorTetrix, openLoop, encoder)
-#pragma config(Motor,  mtr_S1_C2_2,     frontLeft,     tmotorTetrix, openLoop, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C1_1,     backRight,     tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     backLeft,      tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     frontRight,    tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     frontLeft,     tmotorTetrix, PIDControl, reversed, encoder)
 
 #include "2014 Autonomous Functions.h"
 
