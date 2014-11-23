e13a38a359037e147e977f565261674d2a831209
Tyler Nelson <tylernel@live.com>
2014-11-23 11:16:41 -0800

    autonomous 3
    tapan made some changes on the framework for autonomous 3
    
    -tyler

--- RobotC/Autonomous 3.c
+++ RobotC/Autonomous 3.c
@@ -1,5 +1,6 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
 #pragma config(Sensor, S4,     gyro,           sensorI2CHiTechnicGyro)
+#pragma config(Sensor, S2,     touchSensor,            sensorTouch)
 #pragma config(Motor,  mtr_S1_C1_1,     backRight,     tmotorTetrix, PIDControl, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     backLeft,      tmotorTetrix, PIDControl, reversed, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     frontRight,    tmotorTetrix, PIDControl, encoder)
@@ -9,25 +10,29 @@
 
 task main()
 {
+	int x = 23;
 	initializeRobot();
-drive(x);
-if (IR sensor reads that IR is ahead)
-	approach IR;
+	drive(x);
 
+	if (irSeeker.acValues[1] > 10 && irSeeker.acValues[2] > 10)
+		approachIR();
 	else
+  {
 		turn(-x);
 		drive(x);
 		turn(x);
 		drive(x);
+  }
 
-		if (IR sensor reads that IR is right)
-			approach IR;
+	if (irSeeker.acValues[3] > 10 && irSeeker.acValues[4] > 10)
+		approachIR();
 
-		else
-			drive(x);
-			turn(x);
-			drive(x);
-			approach IR
-
+	else
+  {
+		drive(x);
+		turn(x);
+		drive(x);
+		approachIR();
+  }
 
 }
