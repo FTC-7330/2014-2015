f5ce39ab4805779fc47e76884fa027356317b04e
Tyler Nelson <tylernel@live.com>
2015-01-02 11:28:26 -0800

    Center goal stuff
    -tiller

---
+++ RobotC/Center Goal.c
@@ -0,0 +1,47 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  none)
+#pragma config(Sensor, S4,     gyro,           sensorI2CHiTechnicGyro)
+#pragma config(Motor,  mtr_S1_C1_1,     backRight,     tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     backLeft,      tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C2_1,     frontRight,    tmotorTetrix, PIDControl, encoder)
+#pragma config(Motor,  mtr_S1_C2_2,     frontLeft,     tmotorTetrix, PIDControl, reversed, encoder)
+#pragma config(Motor,  mtr_S1_C3_1,     collectionMotor, tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C3_2,     camMotor,      tmotorTetrix, openLoop)
+
+#include "2014 Autonomous Functions.h"
+
+task main()
+{
+	int beaconPosition = getBeaconPosition();
+	if(beaconPosition == 3)
+	{
+		approachIR();
+		//lift thing (credit to brian)
+		//drop the balls in the center goal
+	}
+	else if(beaconPosition == 2)
+	{
+		drive(x);
+		turn(-90, 30);
+		drive(x);
+		turn(60, 30);
+		drive(x);
+		turn(90);
+		approachIR();
+		//lift thing (credit to brian)
+		//drop the balls in the center goal
+	}
+	else // beacon position 1
+	{
+		drive(x);
+		turn(-90, 30);
+		drive(x);
+		turn(60, 30);
+		drive(x);
+		turn(30, 30);
+		drive(x);
+		turn(90, 30);
+		approachIR();
+		//lift thing (credit to brian)
+		//drop the balls in the center goal
+	}
+}
